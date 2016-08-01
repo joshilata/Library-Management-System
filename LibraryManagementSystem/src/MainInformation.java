7c2209f1c2c9556ca7f80ff18c898184ca269730
gopaljoshi <gopaljoshi@Gopal>
2016-08-02 11:33:47 +1200

    modification in main class

--- LibraryManagementSystem/src/MainInformation.java
+++ LibraryManagementSystem/src/MainInformation.java
@@ -2,23 +2,21 @@
 import java.util.Scanner;
 public class MainInformation {
 public static void main(String[]args){
-	System.out.println("Enter ISBN");
+	System.out.println("Enter ISBN = 1234");
 	Scanner scanner=new Scanner(System.in);
 	String str1=scanner.next();
-	System.out.println("Enter Book name");
+	System.out.println("Book name = Java");
 	String str2=scanner.next();
-	System.out.println("Enter author");
+	System.out.println("Enter author = John");
 	String str3=scanner.next();
-	System.out.println("Enter edition");
+	System.out.println("Enter edition = 2");
 	String str4=scanner.next();
-	System.out.println("Enter price");
-	String int5=scanner.next();
 	
 	System.out.println("ISBN"+str1); 
 	System.out.println("name"+str2);
 	System.out.println("author"+str3);
 	System.out.println("edition"+str4);
-	System.out.println("price"+int5);
+	
 }
 
 
