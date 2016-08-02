9a59b204cc54b764a8beb8b3d8bcd1a664090b4b
gopaljoshi <gopaljoshi@Gopal>
2016-08-02 15:02:56 +1200

    changes made to the error

--- LibraryManagementSystem/src/MainInformation.java
+++ LibraryManagementSystem/src/MainInformation.java
@@ -5,17 +5,17 @@ public static void main(String[]args){
 	System.out.println("Enter ISBN");
 	Scanner scanner=new Scanner(System.in);
 	String str1=scanner.next();
-	System.out.println("Book name");
+	System.out.println("Book title");
 	String str2=scanner.next();
-	System.out.println("Enter author");
+	System.out.println("Enter name");
 	String str3=scanner.next();
 	System.out.println("Enter edition");
 	String str4=scanner.next();
 
 	
 	System.out.println("ISBN"+str1); 
-	System.out.println("name"+str2);
-	System.out.println("author"+str3);
+	System.out.println("title"+str2);
+	System.out.println("name"+str3);
 	System.out.println("edition"+str4);
 
 }
